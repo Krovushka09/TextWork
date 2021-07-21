5c9b621b806cd35eb67a4837eb20bed15f2609a2
Vladimir <43116496+Krovushka09@users.noreply.github.com>
2021-07-21 16:33:25 +0500

    Add files via upload
    .

---
+++ somefile.js
@@ -0,0 +1,28 @@
+function addWord (shortEl, longEl) {
+    if (localStorage.getItem('dictionary')==null) {
+        createJSON();
+    }
+    const strObj = localStorage.getItem('dictionary');
+    let jsObj = JSON.parse(strObj);
+    jsObj.dictionary.push({short: shortEl, long: longEl});
+    localStorage.setItem('dictionary', JSON.stringify(jsObj));
+}
+
+function deleteWord (shorlEl) {
+    if (localStorage.getItem('dictionary')==null) {
+        return console.error('словарь пустой, невозможно удалить');
+    }
+    const strObj = localStorage.getItem('dictionary');
+    let jsObj = JSON.parse(strObj);
+    jsObj.dictionary.splice(jsObj.dictionary.indexOf(shorlEl, 0), 1);
+    localStorage.setItem('dictionary', JSON.stringify(jsObj));
+}
+
+function createJSON () {
+    let object = {
+        dictionary: [
+        {short: 'нзч', long: 'не за что'},
+        {short: 'milf', long: 'man i love family'}
+    ]};
+    localStorage.setItem('dictionary', JSON.stringify(object));
+}
